4d2fcdd597c6a631747492278b15185415366fb9
zhu.gd <zhu.gd@newtouch-elec.com>
2021-01-27 15:58:03 +0800

    first commit

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://172.22.113.90:5000");
+            var client =  new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "123" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
